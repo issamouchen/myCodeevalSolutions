aee13b553f508a078b535d41dbade965fe418e69
issam ouchen <issam.oushen@gmail.com>
2016-04-27 12:02:17 -0400

    Create HexToDecimal.cs

---
+++ Easy/HexToDecimal/HexToDecimal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            // do something with line
+            int decValue = Convert.ToInt32(line, 16);
+
+            Console.WriteLine(decValue);
+        }
+    }
+} 
