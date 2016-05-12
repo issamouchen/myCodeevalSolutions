3feb85f9fe1af181c93fc4b0659a850cdd00f555
issam ouchen <issam.oushen@gmail.com>
2016-05-12 16:38:17 -0400

    Create OddNumbers.cs

---
+++ Easy/Odd-Numbers/OddNumbers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       for (int i = 1; i <= 99; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+    }
+} 
