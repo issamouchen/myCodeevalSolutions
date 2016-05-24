ac0436e25c652d09c078dcfa3be573de2530ec10
issam ouchen <issam.oushen@gmail.com>
2016-05-24 08:26:18 -0400

    Update DataRecovery.cs

--- Easy/Data-Recovery/DataRecovery.cs
+++ Easy/Data-Recovery/DataRecovery.cs
@@ -1,33 +1,54 @@
-#Data Recovery
 
- Your friends decided to make fun of you. They have installed a script on your computer which shuffled all words within a text. It is a joke, so they have left hints for each sentence. The hints will allow you to rebuild the data easily, but you need to find out how to use them.
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
 
-Your task is to write a program which reconstructs each sentence out of a set of words and prints out the original sentences.
-Input sample:
 
-Your program should accept a path to a filename as its first argument. Each line is a test case which consists of a set of words and a sequence of numbers separated by a semicolon. The words within a set and the numbers within a sequence are separated by a single space.
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            // do something with line
+             string[] split = line.Split(';');
+                string[] words = split[0].Split(' ');
+                string[] output = new string[words.Length];
+                int[] indexes = split[1].Split(' ').Select(int.Parse).ToArray();
+                for (int i = 0; i < words.Length; i++)
+                {
+                     int outputIndex = -1;
+                    if (i < indexes.Length) 
+                    {
+                        outputIndex = indexes[i] - 1;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < output.Length; j++) 
+                        {
+                            if (output[j] == null) 
+                            {
+                                outputIndex = j;
+                                break;
+                            }
+                        }
+                    }
 
-For example:
+                    output[outputIndex] = words[i];
+                }
 
-2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2
+                var sb = new StringBuilder(split[0].Length + 1);
+                foreach (var word in output)
+                {
+                    sb.AppendFormat("{0} ", word);
+                }
 
-programming first The language;3 2 1
-
-programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9
-
-Output sample:
-
-Print out a reconstructed sentence for each test case, one per line.
-
-For example:
-
-However, it was not implemented until 1998 and 2000
-
-The first programming language
-
-The Manchester Mark 1 ran programs written in Autocode from 1952
-
-Constraints:
-
-    The number of test cases is in a range from 20 to 40.
-    The words consist of ASCII uppercase and lowercase letters, digits, and punctuation marks.
+                Console.WriteLine(sb.ToString(0, sb.Length - 1));
+        }
+    }
+} 
